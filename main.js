ba121c50850d51415b2a5b2aa80afeefb0bbf381
Marek Brzeziński <marek.j.brzezinski@gmail.com>
2022-10-15 18:04:01 +0200

    Simplest element render

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-document.querySelector("#app").innerHTML = `
-  <div>
-    <p>Hello</p>
-  </div>
-`;
+const element = {
+  type: "h1",
+  props: {
+    title: "foo",
+    children: "Hello",
+  },
+};
+
+const container = document.getElementById("app");
+
+const node = document.createElement(element.type);
+node["title"] = element.props.title;
+
+const text = document.createTextNode("");
+text["nodeValue"] = element.props.children;
+
+node.appendChild(text);
+container.appendChild(node);
